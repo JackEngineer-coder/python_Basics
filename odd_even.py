c9c795a928102e65848362f4562c24b56dcb50f5
Jitender Chauhan <66746889+JackEngineer-coder@users.noreply.github.com>
2022-01-31 23:39:27 +0530

    Create odd_even.py

---
+++ odd_even.py
@@ -0,0 +1,7 @@
+# code for odd or even numbeer
+number = int(input("enter your number\n"))
+result = int(number % 2)
+if result == 0:
+  print("you have enterd a even number")
+else:
+  print("you have entered a odd number")
