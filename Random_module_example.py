7db4e83f2298501562ea26841325c4e714c5a429
Jitender Chauhan <66746889+JackEngineer-coder@users.noreply.github.com>
2022-02-01 23:57:38 +0530

    Create Random_module_example.py

---
+++ Random_module_example.py
@@ -0,0 +1,9 @@
+# # list
+# #random Module
+# import random
+# #for getting integer random number between 1 to 10
+# random_number = random.randint(1,10)
+# print(random_number)
+# #for getting  float random number between 1 to 5
+# random_float = random.random()*5
+# print(random_float)
