feb168d6b7b19beb92732689b3e50dfc69341aa8
Jitender Chauhan <66746889+JackEngineer-coder@users.noreply.github.com>
2022-02-02 00:04:28 +0530

    Update Head_Tail.py

--- Head_Tail.py
+++ Head_Tail.py
@@ -1,7 +1,7 @@
 #code for head and tail in python
-# import random 
-# random_number= random.randint(0,1)
-# if random_number ==1:
-#  print("head")
-# else:
-#  print("tail")
+import random 
+random_number= random.randint(0,1)
+if random_number ==1:
+  print("head")
+else:
+  print("tail")
