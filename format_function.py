e9896ddc52c0e9334ecbbbc348a5b0ebf4529f29
Jitender Chauhan <66746889+JackEngineer-coder@users.noreply.github.com>
2022-02-08 20:57:06 +0530

    Create format_function.py

---
+++ format_function.py
@@ -0,0 +1,7 @@
+def formated_name(f_name,L_name):
+  formated_f_name = f_name.title()
+  formated_l_name = L_name.title()
+
+  return f"{formated_f_name} {formated_l_name}" 
+
+print(formated_name("JitU" , "chAuHan"))
